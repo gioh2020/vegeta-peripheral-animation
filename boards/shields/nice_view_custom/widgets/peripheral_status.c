3f2841ccdfcf547d2cab0ddb52298a4b8de8a2f9
gioh2020 <gioh2020@gmail.com>
2026-06-01 16:22:04 -0500

    Replace Mario animation with Vegeta Dragon Ball Z (10 frames)

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -23,53 +23,20 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include "peripheral_status.h"
 
-LV_IMG_DECLARE(mario1standing);
-LV_IMG_DECLARE(mario2run1);
-LV_IMG_DECLARE(mario2run2);
-LV_IMG_DECLARE(mario2run3);
-LV_IMG_DECLARE(mario3jump1);
-LV_IMG_DECLARE(mario3jump2);
-LV_IMG_DECLARE(mario3jump3);
-LV_IMG_DECLARE(mario3jump4);
-LV_IMG_DECLARE(mario3jump5);
-LV_IMG_DECLARE(mario3jump6);
-LV_IMG_DECLARE(mario3jump7);
-LV_IMG_DECLARE(mario3jump8);
-LV_IMG_DECLARE(mario3jump9);
-LV_IMG_DECLARE(mario3jump10);
-LV_IMG_DECLARE(mario3jump11);
-LV_IMG_DECLARE(mario3jump12);
-LV_IMG_DECLARE(mario3jump13);
+LV_IMG_DECLARE(vegeta01);
+LV_IMG_DECLARE(vegeta02);
+LV_IMG_DECLARE(vegeta03);
+LV_IMG_DECLARE(vegeta04);
+LV_IMG_DECLARE(vegeta05);
+LV_IMG_DECLARE(vegeta06);
+LV_IMG_DECLARE(vegeta07);
+LV_IMG_DECLARE(vegeta08);
+LV_IMG_DECLARE(vegeta09);
+LV_IMG_DECLARE(vegeta10);
 
 const lv_img_dsc_t *anim_imgs[] = {
-    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
-    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
-    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
-    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
-    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
-    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
-    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
-    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
-    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
-    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
-    &mario2run1,&mario2run2,&mario2run3,
-    &mario2run1,&mario2run2,&mario2run3,
-    &mario2run1,&mario2run2,&mario2run3,
-    &mario2run1,&mario2run2,&mario2run3,
-    &mario2run1,&mario2run2,&mario2run3,
-    &mario3jump1,
-    &mario3jump2,
-    &mario3jump3,
-    &mario3jump4,
-    &mario3jump5,
-    &mario3jump6,
-    &mario3jump7,
-    &mario3jump8,
-    &mario3jump9,
-    &mario3jump10,
-    &mario3jump11,
-    &mario3jump12,
-    &mario3jump13,
+    &vegeta01, &vegeta02, &vegeta03, &vegeta04, &vegeta05,
+    &vegeta06, &vegeta07, &vegeta08, &vegeta09, &vegeta10,
 };
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
@@ -167,8 +134,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     lv_obj_t * art = lv_animimg_create(widget->obj);            //<--
     lv_obj_center(art);                                         //<--
-    lv_animimg_set_src(art, (const void **) anim_imgs, 78);     //<--
-    lv_animimg_set_duration(art, 7800);                         //<--
+    lv_animimg_set_src(art, (const void **) anim_imgs, 10);     //<--
+    lv_animimg_set_duration(art, 1000);                         //<--
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);  //<--
     lv_animimg_start(art);                                      //<--
 
